808c02b7dff574f8614908dab6d799e548404e58
rickard uttermalm <rickard.uttermalm@gmail.com>
2019-03-13 14:13:18 +0100

    fiddling with autocomplete

--- js/App.js
+++ js/App.js
@@ -2,5 +2,14 @@ Init();
 
 function Init(){
     var test = ["test", "hojme", "knarkblod"];
-    document.getElementById("#countryList")
+    var list = document.getElementById("countryList");
+    console.log(list);
+    test.forEach(function(item) {
+        // Create a new <option> element.
+        var option = document.createElement('option');
+        // Set the value using the item in the JSON array.
+        option.value = item;
+        // Add the <option> element to the <datalist>.
+        list.appendChild(option);
+      });
 }
